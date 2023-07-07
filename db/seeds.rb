2e37804028df3d5a962572b475d005ed6eda4b85
MarianaLioiPerez <lioiperezm@gmail.com>
2023-07-07 10:57:55 -0400

    desafio blockbuster

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+
+5.times do |i|
+    Client.create(name: Faker::Name.name, age:Faker::Number.within(range: 15..90))
+    end
+
+5.times do |i|
+    Movie.create(name: Faker::Movie.title)
+    end
